ff5e6ddd5fcb087ec1cdf67c67d4284ee71dc44f
stedes <stevendesair@gmail.com>
2015-04-04 14:19:54 +0200

    Added file upload and download

--- tempshare-meteor.js
+++ tempshare-meteor.js
@@ -1,9 +1,21 @@
+// MongoDB collection for the rooms
 Rooms= new Mongo.Collection("rooms");
+
+// Storing files with CollectionFS and GridFS
+var imageStore = new FS.Store.GridFS("images");
+Images = new FS.Collection("images",{
+  stores: [imageStore]
+});
+
 
 if (Meteor.isClient) {
   Template.body.helpers({
     rooms: function(){
       return Rooms.find({});
+    },
+
+    images: function(){
+      return Images.find({});
     },
 
     numberOfRooms: function(){
@@ -31,5 +43,19 @@ if (Meteor.isClient) {
     "click .delete": function(event){
       Rooms.remove(this._id)
     }
-  })
+  });
+
+
+  Template.fileUpload.events({
+    "change .myFileInput": function(event, template) {
+      console.log("upload initiated");
+      FS.Utility.eachFile(event, function(file) {
+        Images.insert(file, function (err, fileObj) {
+          if (err){console.log("Error in upload")}
+          else {"Upload successful"};
+        });
+      });
+
+    }
+  });
 }
